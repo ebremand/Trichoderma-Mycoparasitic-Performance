f34a88e7b1e670878342a6e051cc65b6254f2aee
ebremand <etienne.bremand@etud.univ-angers.fr>
2026-02-18 16:29:56 +0100

    Modify script to run OrthoFinder for proteome analysis
    Updated script to run OrthoFinder instead of BUSCO for analyzing proteomes. Adjusted user variables and output directory accordingly.

--- Genomic/4-Genomic_comparison/Orthofinder.sh
+++ Genomic/4-Genomic_comparison/Orthofinder.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
 ###############################################################################
-# Script: Busco.sh
+# Script: OrthoFinder.sh
 # Description:
-#   Run BUSCO to evaluate the completeness of a genome assembly.
+#   Run OrthoFinder to infer orthologous groups from multiple proteomes.
 #
 #   To use:
-#     - Edit the variables below to point to your assembly, output directory, lineage, and sample name.
+#     - Edit the variables below to point to your proteome folder and output directory.
 ###############################################################################
 
 # -------------------------
 # === USER VARIABLES ===
 # -------------------------
-# Sample name
-SAMPLE="sample_name"
+# Folder containing all proteomes in .faa format
+PROTEOMES="/path/to/your/proteomes_folder"
 
-# Path to your genome assembly
-ASSEMBLY="/path/to/your/assembly.fasta"
-
-# Output directory for BUSCO results
-OUTPUT_DIR="./busco_results"
-
-# Path to BUSCO lineage dataset
-LINEAGE="/path/to/busco/lineages/fungi_odb10"
+# Output directory for OrthoFinder results
+OUTPUT_DIR="/path/to/your/orthofinder_results"
 
 # Number of threads to use
 THREADS=10
 
 # -------------------------
-# Run BUSCO
+# Run OrthoFinder
 # -------------------------
-echo "=== Running BUSCO on $SAMPLE ==="
+echo "=== Running OrthoFinder on proteomes in $PROTEOMES ==="
 
-busco -i "$ASSEMBLY" \
-      -o "$SAMPLE" \
-      -l "$LINEAGE" \
-      -m genome \
-      --cpu "$THREADS" \
-      -f \
-      --out "$OUTPUT_DIR"
+# Remove previous output folder to start fresh
+rm -rf "$OUTPUT_DIR/Orthofinder"
 
-echo "BUSCO analysis completed. Results are in $OUTPUT_DIR/$SAMPLE"
+orthofinder -f "$PROTEOMES" \
+            -t "$THREADS" \
+            -a "$THREADS" \
+            -S diamond \
+            -M msa \
+            -o "$OUTPUT_DIR/Orthofinder"
+
+echo "OrthoFinder analysis completed. Results are in $OUTPUT_DIR/Orthofinder"
