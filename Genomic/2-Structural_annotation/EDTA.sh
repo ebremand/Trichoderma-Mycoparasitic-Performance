5c133370f8a4fd8c7ff2984dab06dbe7444fbb34
ebremand <etienne.bremand@etud.univ-angers.fr>
2026-02-18 15:43:41 +0100

    Rename script and update to run EDTA for annotation

--- Genomic/2-Structural_annotation/EDTA.sh
+++ Genomic/2-Structural_annotation/EDTA.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 ###############################################################################
-# Script: Busco.sh
+# Script: EDTA.sh
 # Description:
-#   Run BUSCO to evaluate the completeness of a genome assembly.
+#   Run EDTA to annotate transposable elements in a genome assembly.
 #
 #   To use:
-#     - Edit the variables below to point to your assembly, output directory, lineage, and sample name.
+#     - Edit the variables below to point to your genome assembly and output directory.
 ###############################################################################
 
 # -------------------------
 # === USER VARIABLES ===
 # -------------------------
+
 # Sample name
 SAMPLE="sample_name"
 
-# Path to your genome assembly
-ASSEMBLY="/path/to/your/assembly.fasta"
+# Path to genome assembly (FASTA)
+GENOME="/path/to/your/assembly.fasta"
 
-# Output directory for BUSCO results
-OUTPUT_DIR="./busco_results"
-
-# Path to BUSCO lineage dataset
-LINEAGE="/path/to/busco/lineages/fungi_odb10"
+# Output directory
+OUTPUT_DIR="./EDTA_results"
 
 # Number of threads to use
 THREADS=10
 
-# -------------------------
-# Run BUSCO
-# -------------------------
-echo "=== Running BUSCO on $SAMPLE ==="
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR"
 
-busco -i "$ASSEMBLY" \
-      -o "$SAMPLE" \
-      -l "$LINEAGE" \
-      -m genome \
-      --cpu "$THREADS" \
-      -f \
-      --out "$OUTPUT_DIR"
+# -------------------------
+# === RUN EDTA ===
+# -------------------------
 
-echo "BUSCO analysis completed. Results are in $OUTPUT_DIR/$SAMPLE"
+echo "=== Running EDTA on $SAMPLE ==="
+
+EDTA.pl \
+  --genome "$GENOME" \
+  --species others \
+  --step all \
+  --anno 1 \
+  --threads "$THREADS" \
+  --sensitive 1 \
+  --evaluate 1 \
+  --overwrite 0 \
+  --force 1
+
+echo "EDTA annotation completed."
+echo "Results are available in: $OUTPUT_DIR"
